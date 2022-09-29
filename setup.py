af0f4167992437ea450be92b5c876bb231c952eb
Marcelina Oset <moset@internships.antmicro.com>
2022-09-29 16:18:35 +0200

    [#37726] Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='kenning_plot',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    description="kenning-plot - a tool for drawing time series plots",
+    author='Antmicro Ltd.',
+    author_email='contact@antmicro.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'bokeh>=2.4.3',
+        'numpy>=1.23.0',
+        'plotext>=5.0.2',
+        'selenium>=4.4.0'
+    ],
+)
