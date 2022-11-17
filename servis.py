62981b972635ce6bb9316e0aa257bc50a54a3518
Marcelina Oset <moset@internships.antmicro.com>
2022-11-17 17:09:31 +0100

    [#39036] servis.py: Changed the way of parsing file argument to the one as recommended in agrparse docs

--- servis.py
+++ servis.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
-import fileinput
+import sys
 import numpy as np
 from servis import render_time_series_plot_with_histogram
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('datasrc', metavar='DATA_SOURCE',
-                        type=str, nargs='?', default=None)
+    parser.add_argument('infile', type=argparse.FileType('r'), nargs='?', default=sys.stdin)
     args = parser.parse_args()
 
     x_values = list()
     y_values = list()
 
-    for line in fileinput.input(args.datasrc):
+    for line in args.infile:
         if line == '\n': 
             continue
         y = int(line.split(" ")[0])
@@ -25,13 +24,10 @@ def main():
     if len(x_values) == 0:
         x_values = np.arange(len(y_values))
 
-    print(y_values)
-    print(x_values)
-
     render_time_series_plot_with_histogram(
-        "example_plots/singleplot",
+        "",
         ["txt"],
-        "Example plot",
+        "",
         None,
         None,
         None,
