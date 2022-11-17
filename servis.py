3a106c95dd35cf0d98bab6dbdcf179a1270d00ee
Marcelina Oset <moset@internships.antmicro.com>
2022-11-17 16:47:09 +0100

    [#39036] servis.py: Wrapped code in def main()

--- servis.py
+++ servis.py
@@ -5,36 +5,41 @@ import fileinput
 import numpy as np
 from servis import render_time_series_plot_with_histogram
 
-parser = argparse.ArgumentParser()
-parser.add_argument('datasrc', metavar='DATA_SOURCE',
-                    type=str, nargs='?', default=None)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('datasrc', metavar='DATA_SOURCE',
+                        type=str, nargs='?', default=None)
+    args = parser.parse_args()
 
-x_values = list()
-y_values = list()
+    x_values = list()
+    y_values = list()
 
-for line in fileinput.input(args.datasrc):
-    if line == '\n': 
-        continue
-    y = int(line.split(" ")[0])
-    y_values.append(y)
+    for line in fileinput.input(args.datasrc):
+        if line == '\n': 
+            continue
+        y = int(line.split(" ")[0])
+        y_values.append(y)
 
-y_values = np.array(y_values)
+    y_values = np.array(y_values)
 
-if len(x_values) == 0:
-    x_values = np.arange(len(y_values))
+    if len(x_values) == 0:
+        x_values = np.arange(len(y_values))
 
-print(y_values)
-print(x_values)
+    print(y_values)
+    print(x_values)
 
-render_time_series_plot_with_histogram(
-    "example_plots/singleplot",
-    ["txt"],
-    "Example plot",
-    None,
-    None,
-    None,
-    None,
-    x_values,
-    y_values
-)
+    render_time_series_plot_with_histogram(
+        "example_plots/singleplot",
+        ["txt"],
+        "Example plot",
+        None,
+        None,
+        None,
+        None,
+        x_values,
+        y_values
+    )
+
+
+if __name__ == '__main__':
+    main()
