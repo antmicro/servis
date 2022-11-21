70b00dba9b476a3f6a2130e8bcdc1a9889032de2
Grzegorz Latosinski <glatosinski@antmicro.com>
2022-11-21 13:56:07 +0100

    [#39036] servis: __init__: Added flake8 supressions
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- servis/__init__.py
+++ servis/__init__.py
@@ -1,5 +1,5 @@
 """
 Module for creating and rendering time series plots in various formats
 """
-from servis.time_series import render_time_series_plot_with_histogram
-from servis.time_series import render_multiple_time_series_plot
+from servis.time_series import render_time_series_plot_with_histogram  # noqa: F401, E501
+from servis.time_series import render_multiple_time_series_plot  # noqa: F401
