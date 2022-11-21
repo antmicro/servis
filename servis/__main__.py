0950ad7f9e8af11d0aaf54896c80666abf0f1a7d
Grzegorz Latosinski <glatosinski@antmicro.com>
2022-11-21 14:18:24 +0100

    [#39036] servis: Added possibility to configure plot type and data delimiter
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- servis/__main__.py
+++ servis/__main__.py
@@ -7,8 +7,25 @@ from servis import render_time_series_plot_with_histogram
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType(
-        'r'), nargs='?', default=sys.stdin)
+    parser.add_argument(
+        'infile',
+        type=argparse.FileType('r'),
+        nargs='?',
+        help='Input data stream. Entries are separated by newline, Y and X values are separated by delimiter (space by default)',  # noqa: E501
+        default=sys.stdin
+    )
+    parser.add_argument(
+        '-d',
+        '--delimiter',
+        type=str,
+        help='Y and X values delimiter',
+        default=' '
+    )
+    parser.add_argument(
+        '--plot-type',
+        type=str,
+        default='scatter'
+    )
     args = parser.parse_args()
 
     x_values = list()
@@ -17,8 +34,10 @@ def main():
     for line in args.infile:
         if line == '\n':
             continue
-        y = int(line.split(" ")[0])
-        y_values.append(y)
+        split = line.split(args.delimiter)
+        y_values.append(int(split[0]))
+        if len(split) == 2:
+            x_values.append(int(split[1]))
 
     if len(x_values) == 0:
         x_values = [i for i in range(len(y_values))]
@@ -26,6 +45,10 @@ def main():
     render_time_series_plot_with_histogram(
         y_values,
         x_values,
+        is_x_timestamp=False,
+        trimxvalues=False,
+        skipfirst=False,
+        plottype=args.plot_type
     )
 
 
