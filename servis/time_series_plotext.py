60cbbf9063d44b2f54a7f0674e41f75bd2581943
Marcelina Oset <moset@internships.antmicro.com>
2022-12-15 11:39:54 +0100

    [#39843] servis: time_series_plotext: Added function for rendering multiple ascii plot

--- servis/time_series_plotext.py
+++ servis/time_series_plotext.py
@@ -60,6 +60,7 @@ def create_ascii_plot(
     """
     assert plottype in ['scatter', 'bar', 'line']
     import plotext
+
     plotext.clear_figure()
 
     if is_x_timestamp is True:
@@ -110,3 +111,72 @@ def create_ascii_plot(
     plotext.axes_color(axes_color)
     plotext.ticks_color(ticks_color)
     plotext.show()
+
+
+def create_multiple_ascii_plot(
+        ydatas: List,
+        xdatas: List,
+        titles: List[str],
+        xtitles: List[str],
+        xunits: List[str],
+        ytitles: List[str],
+        yunits: List[str],
+        x_ranges: Optional[List[Tuple]] = None,
+        y_ranges: Optional[List[Tuple]] = None,
+        figsize: Tuple = (1500, 1080),
+        is_x_timestamp: bool = True,
+        plottype: str = 'bar'):
+    """
+    Draws multiple ASCII time series plot in terminal.
+
+    Used i.e. to draw time series plot during CI.
+
+    Parameters
+    ----------
+    ydatas : List
+        The values for Y dimension
+    xdatas : List
+        The values for X dimension
+    titles : Optional[str]
+        Title of the plots
+    xtitles : Optional[str]
+        Names of the X axes
+    xuints : Optional[str]
+        Units for the X axes
+    ytitles : Optional[str]
+        Names of the Y axes
+    yunits : Optional[str]
+        Units for the Y axes
+    x_ranges : Optional[Tuple]
+        The ranges of zoom on X axes
+    y_ranges : Optional[Tuple]
+        The ranges of zoom on Y axes
+    figsize : Optional[Tuple]
+        The size of the figure
+    is_x_timestamp:
+        True if x should be a timestamp,
+        False if x should be converted to datetime
+    plottype : str
+        Can be 'scatter', 'bar' or 'line'
+    """
+
+    print('\n\n')
+    for title, xtitle, xunit, ytitle, yunit, ydata, xdata, \
+        x_range, y_range in zip(
+            titles, xtitles, xunits, ytitles, yunits,
+            ydatas, xdatas, x_ranges, y_ranges):
+        create_ascii_plot(
+            ydata=ydata,
+            xdata=xdata,
+            title=title,
+            xtitle=xtitle,
+            xunit=xunit,
+            ytitle=ytitle,
+            yunit=yunit,
+            x_range=x_range,
+            y_range=y_range,
+            figsize=figsize,
+            plottype=plottype,
+            is_x_timestamp=is_x_timestamp
+        )
+        print('\n\n')
