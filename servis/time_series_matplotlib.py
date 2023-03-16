[#42549] time_series_matplotlib: Added x_ranges and y_ranges parameters, made sure that all titles and labels are displayed, corrected units printing and fixed legend
Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

servis/time_series_matplotlib.py
@@ -6,10 +6,10 @@ import numpy as np
 from servis.utils import validate_colormap, validate_kwargs
 
 rcParams['font.sans-serif'] = 'lato'
+RANGE_BORDER_SCALE = 0.04
 NOT_SUPPORTED_PARAMS = {
-    'x_ranges', 'y_ranges', 'outputext', 'trimxvaluesoffsets',
+    'outputext', 'trimxvaluesoffsets', 'is_x_timestamp',
     'tags', 'tagstype', 'setgradientcolors', 'plottype',
-    'is_x_timestamp'
 }
 
 
@@ -22,6 +22,8 @@ def create_multiple_matplotlib_plot(
         xunits: Optional[List[str]] = None,
         ytitles: Optional[List[str]] = None,
         yunits: Optional[List[str]] = None,
+        x_ranges: Optional[List[Tuple]] = None,
+        y_ranges: Optional[List[Tuple]] = None,
         outpath: Optional[Path] = None,
         figsize: Tuple = (15, 8.5),
         bins: int = 20,
@@ -52,6 +54,10 @@ def create_multiple_matplotlib_plot(
         Name of the Y axis
     yunits : Optional[List[str]]
         Unit for the Y axis
+    x_ranges : Optional[List[Tuple]]
+        The list of ranges of zoom for each X axis
+    y_ranges : Optional[List[Tuple]]
+        The list of ranges of zoom for each Y axis
     outpath : Optional[Path]
         Output path for the plot image. If None, the plot will be displayed.
     figsize : Tuple
@@ -71,47 +77,60 @@ def create_multiple_matplotlib_plot(
 
     plot_colors = validate_colormap(colormap, 'matplotlib', plotsnumber)
     hist_colors = validate_colormap(colormap, 'matplotlib', plotsnumber)
+    if len(legend_labels) > 0:
+        labels = iter(legend_labels)
+    else:
+        labels = iter([None] * plotsnumber)
 
     fig, axs = plt.subplots(
         ncols=2,
         nrows=figsnumber,
         tight_layout=True,
         figsize=figsize,
-        sharey=True,
         gridspec_kw={'width_ratios': (8, 3)}
     )
-    fig.suptitle(title, fontsize='x-large')
+    bbox_extras = []
+
+    bbox_extras.append(fig.suptitle(title, fontsize='x-large'))
     if figsnumber == 1:
         axs = axs[np.newaxis, :]
 
-    for sub_ydatas, sub_xdatas, axplot, axhist in zip(
-            ydatas, xdatas, axs[:, 0], axs[:, 1]):
+    for sub_ydatas, sub_xdatas, y_range, x_range, axplot, axhist in zip(
+            ydatas, xdatas, y_ranges, x_ranges, axs[:, 0], axs[:, 1]):
         axplot.grid()
         axhist.grid(which='both')
         # Drawing points
         for ydata, xdata in zip(sub_ydatas, sub_xdatas):
-            axplot.scatter(xdata, ydata, c=next(plot_colors), alpha=0.5)
+            axplot.scatter(xdata, ydata, color=next(plot_colors),
+                           alpha=0.5, label=next(labels))
         # Drawing histogram
-        y_min = min([min(ydata) for ydata in sub_ydatas])
-        y_max = max([max(ydata) for ydata in sub_ydatas])
+        y_min, y_max = np.min(sub_ydatas), np.max(sub_ydatas)
         axhist.hist(
             sub_ydatas, bins=bins,
             orientation='horizontal', range=(y_min, y_max),
             color=[next(hist_colors) for _ in sub_ydatas])
         # Histogram settings
         axhist.set_xscale('log')
-        axhist.set_xlabel('Value histogram', fontsize='large')
         plt.setp(axhist.get_yticklabels(), visible=False)
+        # Set ranges for OX and OY
+        if y_range is None:
+            border = (y_max - y_min) * RANGE_BORDER_SCALE
+            y_range = (y_min - border, y_max + border)
+        axplot.set_ylim(*y_range)
+        axhist.set_ylim(*y_range)
+        if x_range is None:
+            x_min, x_max = np.min(sub_xdatas), np.max(sub_xdatas)
+            border = (x_max - x_min) * RANGE_BORDER_SCALE
+            x_range = (x_min - border, x_max + border)
+        axplot.set_xlim(*x_range)
 
     # Adding legend
-    legend = None
-    if len(legend_labels) > 1:
-        legend = fig.legend(
-            legend_labels,
+    if len(legend_labels) > 0:
+        bbox_extras.append(fig.legend(
             bbox_to_anchor=[.5, 0.],
             ncols=3,
-            loc="upper center"
-        )
+            loc="upper center"))
+
     # Adding sub-titles
     for subtitle, axplot in zip(subtitles, axs[:, 0]):
         axplot.set_title(subtitle)
@@ -120,11 +139,15 @@ def create_multiple_matplotlib_plot(
     xlabels = None
     if xtitles:
         xlabels = xtitles
-        for i, (xunit, xlabel, axplot) in enumerate(
-                zip(xunits, xlabels, axs[:, 0])):
-            if xunits is not None:
+        for i, (xunit, xlabel, axplot, axhist) in enumerate(
+                zip(xunits, xlabels, axs[:, 0], axs[:, 1])):
+            if xlabel is None:
+                continue
+            if xunit is not None:
                 xlabel += f' [{xunit}]'
-            axplot.set_xlabel(xlabel, fontsize='large')
+            bbox_extras.extend(
+                [axplot.set_xlabel(xlabel, fontsize='large'),
+                 axhist.set_xlabel('Value histogram', fontsize='large')])
 
     # Adding y-labels
     ylabels = None
@@ -132,15 +155,17 @@ def create_multiple_matplotlib_plot(
         ylabels = ytitles
         for i, (yunit, ylabel, axplot) in enumerate(
                 zip(yunits, ylabels, axs[:, 0])):
-            if yunits is not None:
+            if ylabel is None:
+                continue
+            if yunit is not None:
                 ylabel += f' [{yunit}]'
-            axplot.set_ylabel(ylabel, fontsize='large')
+            bbox_extras.append(axplot.set_ylabel(ylabel, fontsize='large'))
 
     if outpath is None:
         plt.show()
     else:
         plt.savefig(outpath,
-                    bbox_extra_artists=(legend,) if legend else None,
+                    bbox_extra_artists=bbox_extras,
                     bbox_inches='tight')
     plt.close()
 
@@ -153,6 +178,8 @@ def create_matplotlib_plot(
         xunit: str,
         ytitle: str,
         yunit: str,
+        x_range: Optional[Tuple] = None,
+        y_range: Optional[Tuple] = None,
         outpath: Optional[Path] = None,
         figsize: Tuple = (15, 8.5),
         bins: int = 20,
@@ -180,6 +207,10 @@ def create_matplotlib_plot(
         Name of the Y axis
     yunit : str
         Unit for the Y axis
+    x_range : Optional[Tuple]
+        The range of zoom for each X axis
+    y_range : Optional[Tuple]
+        The range of zoom for each Y axis
     outpath : Optional[Path]
         Output path for the plot image. If None, the plot will be displayed.
     figsize : Tuple
@@ -200,6 +231,8 @@ def create_matplotlib_plot(
         [xunit],
         [ytitle],
         [yunit],
+        [x_range],
+        [y_range],
         outpath,
         figsize,
         bins,