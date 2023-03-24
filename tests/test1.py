f9284ae67a7f4c186a56fd936a0742292fce4c15
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-03-24 17:19:08 +0100

    [#42553] tests: Restored test1 and test2 so they're consistent with README
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- tests/test1.py
+++ tests/test1.py
@@ -1,9 +1,19 @@
-from utils import data1 as data, OUTPATH_PREFIX, get_tags
+import json
 from servis import render_time_series_plot_with_histogram
+
+with open("data1.json", "r") as data:
+    data = json.load(data)
 
 xdata = data["xdata"]
 ydata = data["ydata"]
-tags = get_tags(data, 'double')
+
+tags = []
+for tag in data["tags"]:
+    tags.append({
+        'name':  tag[0],
+        'start': float(tag[1]),
+        'end':   float(tag[2])
+    })
 
 render_time_series_plot_with_histogram(
     ydata,
@@ -13,7 +23,7 @@ render_time_series_plot_with_histogram(
     "unit",
     "Y axis",
     "unit",
-    outpath=f"{OUTPATH_PREFIX}singleplot",
+    outpath="example_plots/singleplot",
     outputext=["txt", "html", "png", "svg"],
     tags=tags,
     tagstype="double",
--- tests/test2.py
+++ tests/test2.py
@@ -1,10 +1,19 @@
+import json
 from servis import render_multiple_time_series_plot
-from utils import data2 as data, OUTPATH_PREFIX, get_tags
+
+with open("data2.json", "r") as data:
+    data = json.load(data)
 
 xdata = data["xdata"]
 ydata1 = data["ydata1"]
 ydata2 = data["ydata2"]
-tags = get_tags(data)
+
+tags = []
+for tag in data["tags"]:
+    tags.append({
+        'name':  tag[0],
+        'timestamp': float(tag[1])
+    })
 
 render_multiple_time_series_plot(
     [ydata1, ydata2],
@@ -16,8 +25,8 @@ render_multiple_time_series_plot(
     ytitles=["Y axis"]*2,
     yunits=["unit"]*2,
     y_ranges=[(-5, 105)]*2,
-    outpath=f"{OUTPATH_PREFIX}multipleplot",
-    outputext=["png", "html"],
+    outpath="example_plots/multipleplot",
+    outputext=["txt", "html"],
     tags=[tags]*2,
     tagstype="single",
     backend="matplotlib",
